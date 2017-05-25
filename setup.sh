795d107749750788dbee3adde87e115b3fd30a2f
bob-hirosky <bob.hirosky@gmail.com>
2017-05-25 17:41:49 -0400

    add setup script

---
+++ setup.sh
@@ -0,0 +1,42 @@
+# add directory to a path variable
+add_to_envVar () {
+  ADDPATH=$1
+  VAR=$2
+  ERR=0
+  if [ -z $ADDPATH ] || [ ! -d $ADDPATH  ] ; then
+    echo "Path undefined"
+    return 1
+  fi
+  if [ -z $VAR ] ; then
+    echo "Variable undefined"
+    return 1
+  fi
+
+  eval PVAR=\$$VAR
+  if [ -z $PVAR ] ; then
+      export ${VAR}=$ADDPATH
+      echo creating $PVAR \$$VAR
+      return 0
+  fi
+  
+  path_list=`echo $PVAR | tr ':' ' '`
+  for d in $path_list ; do
+      if [ $d == $ADDPATH ] ; then
+	  return 0
+      fi
+  done
+  export ${VAR}=${ADDPATH}:${PVAR}
+}
+
+export PULSEANA=$PWD
+
+
+add_to_envVar ${PULSEANA}/src    PATH
+
+
+
+# hack until someone figures out how to check for root-config in the Makefile
+if [ -e /usr/bin/root ] ; then
+  if [ -z $ROOTSYS ] ; then export ROOTSYS="." ; fi
+fi
+
